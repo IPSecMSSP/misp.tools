ca793e320dd68d4731a71cb7e580e1b9ad8b3f0a
John Berkers <jberkers@ipsec.com.au>
2022-07-05 18:00:32 +1000

    Add functions: - Search-MispEvent - Get-MispAttribute - Search-MispAttribute

--- src/public/attribute/Get-MispAttribute.ps1
+++ src/public/attribute/Get-MispAttribute.ps1
@@ -2,11 +2,11 @@
 function Get-MispEvent {
     <#
     .SYNOPSIS
-        Get MISP Event(s)
+        Get MISP Attribute(s)
     .DESCRIPTION
-        Get MISP Event(s)
+        Get MISP Attribute(s)
 
-        If no Event ID is supplied, all events are returned
+        If no Attribute ID is supplied, all attributes are returned
     .PARAMETER Context
         PSCustomObject with containing the MISP Context
     .PARAMETER Id
@@ -15,16 +15,16 @@ function Get-MispEvent {
         [PsCustomObject]    -> Context
         [Int]               -> Id
     .OUTPUTS
-        [Array]             -> Array of Events
+        [Array]             -> Array of Attributes
     .EXAMPLE
-        PS> $Events = Get-MispEvent -Context $MispContext
-        Return first all Events
+        PS> $Attributes = Get-MispAttribute -Context $MispContext
+        Return all Attributes
     .EXAMPLE
-        PS> $Event = Get-MispEvent -Context $MispContext -Id 1234
-        Return details for event 1234
+        PS> $Event = Get-MispAttribute -Context $MispContext -Id 1234
+        Return details for attribute 1234
     .LINK
         https://url.to.repo/repo/path/
-        https://www.circl.lu/doc/misp/automation/#events-management
+        https://www.circl.lu/doc/misp/automation/#attribute-management
     #>
 
   [CmdletBinding()]
@@ -40,15 +40,15 @@ function Get-MispEvent {
   Begin {
     $Me = $MyInvocation.MyCommand.Name
 
-    Write-Verbose "$($Me): Get MISP Event(s)"
+    Write-Verbose "$($Me): Get MISP Attribute(s)"
 
     # If we don't "Clone" the UriBuilder object from the Context, the Context's instance of the BaseUri gets updated. We do not want that.
     $Uri = [System.UriBuilder]$Context.BaseUri.ToString()
-    $Uri.Path = [io.path]::combine($Uri.Path, "events")
+    $Uri.Path = [io.path]::combine($Uri.Path, "attributes")
 
     # Append the Event Id if requested
     if ($MyInvocation.BoundParameters.ContainsKey("Id")) {
-      $Uri.Path = [io.path]::combine($Uri.Path, $Id)
+      $Uri.Path = [io.path]::combine($Uri.Path, "view/$($Id)")
     }
   }
 
@@ -59,7 +59,7 @@ function Get-MispEvent {
 
     if ($MyInvocation.BoundParameters.ContainsKey("Id")) {
       # Only a single event was requested
-      Write-Output $Response.Event
+      Write-Output $Response.Attribute
     } else {
       # Return all fo the events
       Write-Output $Response
